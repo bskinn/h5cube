72b5cd6c8de5ad3d5ba93bddd43739855d172803
Brian Skinn <brian.skinn@gmail.com>
2016-09-01 00:29:31 -0400

    ADMIN/TEST: cube conversion framework
    TEST:
    
    * First-pass file manipulation framework for pulling .cube's from
      'resource' to 'scratch' and then cleaning up 'scratch' afterward
      is in place
    * Basic test for cube_to_h5 conversion is done, with no arguments
      passed but the path
    
    ADMIN:
    
    * .coveragerc copied in from opan and modified to fit h5cube
    * .checkignore copied in from opan; could set up QuantifiedCode

--- h5cube/test/h5cube_functions.py
+++ h5cube/test/h5cube_functions.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------
 # Name:        h5cube_functions
-# Purpose:     Tests for the (de)compression functionality
+# Purpose:     Tests for the API functions
 #
 # Author:      Brian Skinn
 #                bskinn@alum.mit.edu
@@ -37,10 +37,76 @@ class TestFunctionsMisc(ut.TestCase):
         self.assertRaises(TypeError, _ef, ValueError(), 2)
 
 
+class TestFunctionsCubeToH5(ut.TestCase):
+    import os
+
+    scrpath = os.path.join('h5cube', 'test', 'scratch')
+    respath = os.path.join('h5cube', 'test', 'resource')
+
+    names = {'grid20'}
+    sizes_noargs = {'grid20': 38180}
+
+    delta = 100 # bytes filesize match window
+
+    @staticmethod
+    def shortsleep():
+        from time import sleep
+        sleep(0.1)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.longMessage = True
+
+    def setUp(self):
+        import os
+        import shutil
+
+        for fn in [fn for fn in os.listdir(self.respath)
+                  if fn.endswith('.cube')]:
+            print(fn)
+            shutil.copy(os.path.join(self.respath, fn),
+                        os.path.join(self.scrpath, fn))
+
+        # To ensure filesystem caching &c. can catch up
+        self.shortsleep()
+
+    def tearDown(self):
+        import os
+
+        for fn in os.listdir(self.scrpath):
+            os.remove(os.path.join(self.scrpath, fn))
+
+        # To ensure filesystem caching &c. can catch up
+        self.shortsleep()
+
+    def test_FxnCubeToH5_NoArgs(self):
+        import os
+        from h5cube import cube_to_h5
+
+        for fn in [fn for fn in os.listdir(self.scrpath)
+                   if fn.endswith('.cube')]:
+            # Should only capture files present at the start of fxn execution
+            try:
+                # Call with all defaults
+                cube_to_h5(os.path.join(self.scrpath, fn))
+            except Exception:
+                self.fail("Conversion failed on '{0}'".format(fn))
+
+            h5path = os.path.splitext(fn)[0] + '.h5cube'
+            h5path = os.path.join(self.scrpath, h5path)
+            self.assertAlmostEqual(os.path.getsize(h5path),
+                                   self.sizes_noargs[os.path.splitext(fn)[0]],
+                                   delta=self.delta, # bytes +/-
+                                   msg="Unexpected filesize: {0}"
+                                       .format(h5path))
+            self.shortsleep()
+
+
 def suite():
     s = ut.TestSuite()
     tl = ut.TestLoader()
-    s.addTests([tl.loadTestsFromTestCase(TestFunctionsMisc)])
+    s.addTests([tl.loadTestsFromTestCase(TestFunctionsMisc),
+                tl.loadTestsFromTestCase(TestFunctionsCubeToH5)])
 
     return s
 
