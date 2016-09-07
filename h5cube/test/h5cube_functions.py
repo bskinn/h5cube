1d06fcf3a9562a20b7ca3a81981634022fc31508
Brian Skinn <brian.skinn@gmail.com>
2016-09-07 16:09:32 -0400

    TEST: Add cases
    * Add three should-work compression cases (minmax, isofactor, trunc/isofactor)
    * Add case for deleting source on compression

--- h5cube/test/h5cube_functions.py
+++ h5cube/test/h5cube_functions.py
@@ -43,9 +43,11 @@ class TestFunctionsCubeToH5(ut.TestCase):
     scrpath = os.path.join('h5cube', 'test', 'scratch')
     respath = os.path.join('h5cube', 'test', 'resource')
 
-    names = {'grid20'}
     sizes_noargs = {'grid20': 38180}
     sizes_t2 = {'grid20': 28180}
+    sizes_m1e_8m10 = {'grid20': 23685}
+    sizes_i0x002f4 = {'grid20': 17134}
+    sizes_t8_i0x002f10 = {'grid20': 18964}
 
     delta = 100 # bytes filesize match window
 
@@ -56,7 +58,13 @@ class TestFunctionsCubeToH5(ut.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        import os
+
         cls.longMessage = True
+
+        # Ensure scratch directory exists
+        if not os.path.isdir(cls.scrpath):
+            os.mkdir(cls.scrpath)
 
     def setUp(self):
         import os
@@ -64,7 +72,6 @@ class TestFunctionsCubeToH5(ut.TestCase):
 
         for fn in [fn for fn in os.listdir(self.respath)
                   if fn.endswith('.cube')]:
-            print(fn)
             shutil.copy(os.path.join(self.respath, fn),
                         os.path.join(self.scrpath, fn))
 
@@ -92,7 +99,7 @@ class TestFunctionsCubeToH5(ut.TestCase):
                 # Call with indicated arguments
                 cube_to_h5(os.path.join(self.scrpath, fn), **kwargs)
             except Exception:
-                self.fail("Conversion failed on '{0}'".format(fn))
+                self.fail(msg="Conversion failed on '{0}'".format(fn))
 
             h5path = os.path.splitext(fn)[0] + '.h5cube'
             h5path = os.path.join(self.scrpath, h5path)
@@ -101,13 +108,46 @@ class TestFunctionsCubeToH5(ut.TestCase):
                                    delta=self.delta, # bytes +/-
                                    msg="Unexpected filesize: {0}"
                                        .format(h5path))
-            self.shortsleep()
+            self.shortsleep() # Ensure filesystem is done working
 
     def test_FxnCubeToH5_NoArgs(self):
-        self.basetest_FxnCubeToH5(self.sizes_noargs)
+        self.basetest_FxnCubeToH5(sizes=self.sizes_noargs)
 
     def test_FxnCubeToH5_Trunc2(self):
-        self.basetest_FxnCubeToH5(self.sizes_t2, trunc=2)
+        self.basetest_FxnCubeToH5(sizes=self.sizes_t2, trunc=2)
+
+    def test_FxnCubeToH5_Minmax1e_8_10(self):
+        self.basetest_FxnCubeToH5(sizes=self.sizes_m1e_8m10,
+                                  thresh=True, minmax=[1e-8, 10])
+
+    def test_FxnCubeToH5_Isofactor0x002_4(self):
+        self.basetest_FxnCubeToH5(sizes=self.sizes_i0x002f4,
+                                  thresh=True, isofactor=[0.002, 4])
+
+    def test_FxnCubeToH5_Trunc8_Isofactor0x002_10(self):
+        self.basetest_FxnCubeToH5(sizes=self.sizes_t8_i0x002f10,
+                                  thresh=True, isofactor=[0.002, 10],
+                                  trunc=8)
+
+    def test_FxnCubeToH5_Delete(self):
+        import os
+        from h5cube import cube_to_h5
+
+        fn = os.path.join(self.scrpath, "grid20.cube")
+
+        # Extra confirmation that the file exists prior to processing
+        self.assertTrue(os.path.isfile(fn))
+
+        # Just test the one grid20.cube for post-compress deletion
+        try:
+            cube_to_h5(fn, delsrc=True)
+        except Exception:
+            self.fail(msg="Conversion failed on '{0}'".format(
+                      fnbase + ".cube"))
+
+        # Confirm source file was deleted
+        self.assertFalse(os.path.isfile(fn))
+
 
 def suite():
     s = ut.TestSuite()
